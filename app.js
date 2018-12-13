81655e064340d80fb33d55ab9197aa7c8dfd2298
Ivan <ivan.rahn@gmail.com>
2018-12-13 21:20:20 +1100

    add middleware

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express =require("express");
 const morgan = require("morgan");
+const { errors } = require("celebrate");
 
 const app = express();
 
 app.set('view engine', 'pug');
-
 app.use(express.static("./public"));
+
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.use(morgan("combined"));
 
 app.use(require("./routes"))
 
-
+app.use(errors());
 module.exports = app;
